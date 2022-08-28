8686a22f7646b599da8d6307feee9719f513c675
Kirat <45176400+kirat692157@users.noreply.github.com>
2022-08-28 19:40:48 -0400

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xE069c47408d43A97278eD0934b2E220d004F19FF'
+  'add hex'
 );
 
 export default instance;
